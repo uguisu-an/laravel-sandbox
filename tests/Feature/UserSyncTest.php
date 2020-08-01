bb91881f47b1cf235b8aaf52b4f67566db79ddaa
uguisu-an <arcana278@gmail.com>
2020-08-01 10:38:22 +0900

    add comments

--- tests/Feature/UserSyncTest.php
+++ tests/Feature/UserSyncTest.php
@@ -8,12 +8,17 @@ use Tests\TestCase;
 
 class UserSyncTest extends TestCase
 {
-    // TODO 成功すると200
     // TODO フルシンクとインクリメンタルシンクでテストケース分けてもいいかも
     // TODO productsが取れる
     // TODO product_backlogsが取れる
-    // TODO last_full_syncが取れる
+    // TODO full_sync_beforeが取れる
     // TODO update_countが取れる
+    // TODO last_sync_timeを送れる
+    // TODO last_update_countを送れる
+    // TODO full_sync_beforeがlast_sync_timeより後ならフルシンクする
+    // TODO それ以外はインクリメントシンクする
+    // TODO last_update_countとupdate_countが同じなら何も返さない
+    // TODO last_update_countがupdate_countよりも小さければlast_update_countより後のデータだけを返す
 
     public function test_成功するとOKが返ってくる()
     {
@@ -21,7 +26,6 @@ class UserSyncTest extends TestCase
 
         $response->assertOk();
     }
-
 
     protected function sync()
     {
